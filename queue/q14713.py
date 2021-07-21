2b7ffa11c63c1593e1726ea9fd3db1147e8ae1ab
JuHyun-K <tj52sh9@gmail.com>
2021-07-21 17:30:57 +0900

    설명추가

--- queue/q14713.py
+++ queue/q14713.py
@@ -3,27 +3,29 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n = int(input())
-sentences = [deque(input().rstrip().split()) for _ in range(n)]
-L = deque(input().rstrip().split())
+n = int(input())  # 앵무새 마리 수
+sentences = [deque(input().rstrip().split()) for _ in range(n)]  # 앵무새가 말하는 문장을 담은 큐들의 리스트
+L = deque(input().rstrip().split())  # 체크할 문장(큐)
 
 
+# L로 검사
 while L:
-    top = L.popleft()
+    top = L.popleft()  # 맨 앞 단어부터
 
-    for i in range(len(sentences)):
-        if sentences[i] and top == sentences[i][0]:
+    for i in range(len(sentences)):  # 앵무새 문장의 집합에서 맨 앞 단어들을 pop
+        if sentences[i] and top == sentences[i][0]:  # L.top과 같으면 pop -> possible -> break
             sentences[i].popleft()
             ans = "Possible"
             break
-        else:
+        else:  # 다르면 impossible
             ans = "Impossible"
+
     if ans == "Impossible":
         break
 
-# L이 끝났는데, 앵무새가(sentences)가 말을 다 못하면 실패임
+# L이 끝났는데, 앵무새가(sentences)가 말을 다 못하면 실패
 for i in range(len(sentences)):
-    if sentences[i]:
+    if sentences[i]: # 남아있는 단어가 있으면 -> 실패
         ans = "Impossible"
         break
 
--- queue/q21775.py
+++ queue/q21775.py
@@ -2,15 +2,17 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
+
 n, t = map(int, input().split())
 turns = deque(map(int, input().split()))
-queue = list(deque() for _ in range(n+1))
-cards = set()
+queue = list(deque() for _ in range(n+1))  # 이전에 사용하지 않은 카드가 있으면 저장
+cards = set()  # 공용공간에 없는 숫자카드들
 result = list()
 
 while turns:
-    # 내 큐에 연산카드가 있는 경우 vs. 없는 경우
     turn = turns.popleft()
+    # 내 큐에 연산카드가 있는 경우 vs. 없는 경우
+    # card = [id, 연산, (숫자)]
     card = queue[turn].popleft() if queue[turn] else input().rstrip().split()
 
     if card[1] == 'next':
@@ -19,7 +21,7 @@ while turns:
         cards = cards - {card[2]}
     else:  # acquire
         if card[2] in cards:  # 카드가 공용공간에 x(가져올 수 x)
-             queue[turn].append(card)
+            queue[turn].append(card)
         else:  # 카드를 가져올 수 o
             cards.add(card[2])
 
