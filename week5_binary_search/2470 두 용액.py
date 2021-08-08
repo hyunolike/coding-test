21b23c6416412f6548b488bc9255f49138c5ae58
ghgit1798 <ghgoo1798@gmail.com>
2021-08-08 16:01:06 +0900

    2470 두 용액 코드 변경

--- week5_binary_search/2470 두 용액.py
+++ week5_binary_search/2470 두 용액.py
@@ -1,14 +1,14 @@
 n = int(input())
 liq = list(map(int, input().split()))
 liq.sort()
-left, right, l, r = liq[0], liq[-1], 0, n-1
+l, r = 0, n-1
 flag = False
 a, b, answer = 0, 0, 1000000001
 
-if left<0 and right<0:
+if liq[l]<0 and liq[r]<0:
     print(liq[-2], liq[-1])
     exit()
-elif left>0 and right >0:
+elif liq[l]>0 and liq[r] >0:
     print(liq[0], liq[1])
     exit()
 else:
