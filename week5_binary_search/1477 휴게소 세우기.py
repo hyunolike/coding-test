b96c205d0ba4d0ddcd66f72ed2364f5c676267c9
ghgit1798 <ghgoo1798@gmail.com>
2021-08-10 11:59:08 +0900

    Day 26 [이분탐색] 1. 1477 휴게소 세우기 2. 1654 랜선 자르기

---
+++ week5_binary_search/1477 휴게소 세우기.py
@@ -0,0 +1,23 @@
+n, m, distance = map(int, input().split())
+conv = list(map(int, input().split()))
+conv.append(distance)
+conv.sort()
+
+left, right = 0, distance-1
+answer = 0
+
+while left<=right:
+    mid = (left+right)//2
+    current, cnt = 0, 0
+
+    for position in conv:
+        diff = position - current
+        current = position
+        if diff > mid: # 최대 거리보다 크므로 지어야함
+            cnt += (diff-1)//mid
+    if cnt > m: # m보다 많이 지음. 너무 가까이 지음
+        left = mid+1
+    else: # m보다 작거나 같게 지음. 너무 멀리 지음
+        right = mid-1
+        answer = mid 
+print(answer)
