2bdcaa278d1163a07a1bba9d0c3690f4046ad190
ghgit1798 <ghgoo1798@gmail.com>
2021-08-13 22:47:49 +0900

    Day29

--- week6_dfs/1987 알파벳.py
+++ week6_dfs/1987 알파벳.py
@@ -34,19 +34,19 @@ def BFS_solution():
         q = set([(x, y, board[x][y])])
 
         while q:
-            x, y, ans = q.pop()
+            x, y, alphas = q.pop()
 
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                if ((0 <= nx < R) and (0 <= ny < C)) and (board[nx][ny] not in ans):
-                    q.add((nx,ny,ans + board[nx][ny]))
-                    answer = max(answer, len(ans)+1)
+                if ((0 <= nx < R) and (0 <= ny < C)) and (board[nx][ny] not in alphas):
+                    q.add((nx,ny,alphas + board[nx][ny]))
+                    answer = max(answer, len(alphas)+1)
 
 
     R, C = map(int, input().split())
-    board = [list(map(int, input())) for _ in range(R)]
+    board = [list(map(str, input())) for _ in range(R)]
 
     answer = 1
     BFS(0, 0)
