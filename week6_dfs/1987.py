5c30d1bb0e2c608e7609b8ce3c0a2c191617a4c0
math0917 <math0917@naver.com>
2021-08-25 23:54:56 +0900

    DAY 33

--- week6_dfs/1987.py
+++ week6_dfs/1987.py
@@ -1,17 +1,18 @@
 import sys
 
 
-def per(idx,length):
+def per(idx,length,string):
     global result
     count = 0
     for i in range(4): 
         row = idx[0] + dx[i]
         col = idx[1] + dy[i]
             
-        if 0<=row<=n-1 and 0<=col<=n-1 and not oi[arr[row][col]]:
-            oi[arr[row][col]]= 1
-            per([row,col],length+1)
-            oi[arr[row][col]]= 0
+        if 0<=row<=n-1 and 0<=col<=m-1 and not oi[ord(arr[row][col])-65] and (row,col,string + arr[row][col]) not in visited:
+            oi[ord(arr[row][col])-65]= 1
+            visited.add((row,col,string+arr[row][col]))
+            per([row,col],length+1,string + arr[row][col])
+            oi[ord(arr[row][col])-65]= 0
             count += 1
         if not count:
             result = max(result, length)
@@ -21,15 +22,18 @@ def per(idx,length):
         
 n, m = map(int,sys.stdin.readline().split())
 
-arr = [list(map(lambda x: ord(x)-65,sys.stdin.readline().strip())) for _ in range(n)]
+arr = [list(sys.stdin.readline().strip()) for _ in range(n)]
 
 
 result = 0
 dx = [-1,1,0,0]
 dy = [0,0,1,-1]
 oi = [0]*26
-oi[arr[0][0]] = 1
+oi[ord(arr[0][0]) - 65] = 1
 
+visited = set()
 
-per([0,0],1)
+visited.add((0,0,arr[0][0]))
+
+per([0,0],1,arr[0][0])
 print(result)
