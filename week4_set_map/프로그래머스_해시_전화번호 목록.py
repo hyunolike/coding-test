f8c7e0f8c0a48e39f57aed12d33dd01546ff8230
ghgit1798 <ghgoo1798@gmail.com>
2021-09-05 11:40:39 +0900

    프로그래머스 해시 - 전화번호 목록

--- week4_set_map/프로그래머스_해시_전화번호 목록.py
+++ week4_set_map/프로그래머스_해시_전화번호 목록.py
@@ -1,4 +1,4 @@
-def solution(phone_book):
+def solution1(phone_book):
     answer = True
     phone_book = sorted(phone_book, key=len)
     
@@ -9,3 +9,29 @@ def solution(phone_book):
                 return answer
     
     return answer
+
+def solution2(phone_book):
+    phone_book.sort()
+    
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    
+    return True
+
+
+from collections import defaultdict
+def solution3(phone_book):
+    hash_map = defaultdict(int)
+    
+    for p in phone_book:
+        hash_map[p] += 1
+    
+    for phone_number in phone_book:
+        tmp = ''
+        for t in phone_number:
+            tmp += t
+            if tmp in hash_map.keys() and tmp != phone_number:
+                return False
+    
+    return True
