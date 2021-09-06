4a7f901743283c1fed2a10d1b52bdda4c37cc428
ghgit1798 <ghgoo1798@gmail.com>
2021-09-06 16:54:22 +0900

    Day48

--- week9_simulation/13335 트럭.py
+++ week9_simulation/13335 트럭.py
@@ -2,13 +2,12 @@ from collections import deque
 
 n, w, l = map(int, input().split())
 trucks = deque(list(map(int, input().split())))
-sec = 0
 bridge = deque()
+sec = 0
 while trucks:
     sec += 1
-    total = 0
-    for weight, _ in bridge:
-        total += weight
+    total = sum([weight for weight, _ in bridge])
+    
     if total+trucks[0] <= l and len(bridge) < w:
         go = trucks.popleft()
         bridge.append([go, 1])
@@ -23,4 +22,6 @@ while bridge:
         bridge[i][1] += 1
     if bridge[0][1] == w+1:
         bridge.popleft()
+# 1초를 추가하는 이유
+# 맨 앞 트럭이 다음 트럭이 들어 올 수 있도록 미리 빠져나가므로, 1초 일찍 종료됨
 print(sec+1)
