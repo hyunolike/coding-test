fa288c71173b5daac5dc4747b8eed37ee1531553
ghgit1798 <ghgoo1798@gmail.com>
2021-09-11 23:12:19 +0900

    프로그래머스 로또의 최고 순위, 최저 순위

---
+++ programmers_1/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,41 @@
+def solution(lottos, win_nums):
+    answer = []
+    
+    length, zero, cnt = len(lottos), 0, 0
+    for l in lottos:
+        if l == 0:
+            zero += 1
+    
+    for num in lottos:
+        for win in win_nums:
+            if num == win:
+                cnt += 1
+    
+    best = cnt + zero
+    worst = cnt
+
+    if best < 2: 
+        best = 6
+    else:
+        best = 1 + length - best
+    if worst < 2: 
+        worst = 6
+    else:
+        worst = 1 + length - worst        
+    
+    answer.append(best)
+    answer.append(worst)
+    return answer
+
+def solution(lottos, win_nums):
+    answer=[]
+    rank=[6,6,5,4,3,2,1]
+    matched=0
+    zero = lottos.count(0)
+    for num in lottos:
+        if num in win_nums:
+            matched += 1
+    answer.append(rank[matched+zero])
+    answer.append(rank[matched])
+    
+    return answer
