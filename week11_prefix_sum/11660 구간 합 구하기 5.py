9d235f2604ec4341d80ea647f52754092d814997
ghgit1798 <ghgoo1798@gmail.com>
2021-09-21 09:43:01 +0900

    Day59

--- week11_prefix_sum/11660 구간 합 구하기 5.py
+++ week11_prefix_sum/11660 구간 합 구하기 5.py
@@ -22,3 +22,34 @@ for _ in range(k):
         else:
             total += (nums[i][ey]-nums[i][sy-1])
     print(total)
+
+
+################################
+# 풀이2
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+nums = [[0]*(n+1)]
+
+for _ in range(n):
+    numbers = [0]+list(map(int, input().split()))
+    nums.append(numbers)
+
+# 1. 행 별로 더하기
+for i in range(1, n+1):
+    for j in range(2, n+1):
+        nums[i][j] += nums[i][j-1]
+
+# 2. 열 별로 더하기
+for i in range(1, n+1):
+    for j in range(2, n+1):
+        nums[j][i] += nums[j-1][i]
+
+for _ in range(m):
+    a,b,c,d = map(int, input().split())
+    # (x1,y1)에서 (x2,y2)까지의 합
+    # nums[x2][y2]-nums[x1-1][y2]-nums[x2][y1-1]+nums[x1-1][y1-1]
+    answer = nums[c][d]-nums[a-1][d]-nums[c][b-1]+nums[a-1][b-1]
+    print(answer)
