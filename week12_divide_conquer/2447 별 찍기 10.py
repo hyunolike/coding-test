2c8429b486aa8a754e27f00471fbf19153ac7514
ghgit1798 <ghgoo1798@gmail.com>
2021-09-25 08:18:34 +0900

    Day62

---
+++ week12_divide_conquer/2447 별 찍기 10.py
@@ -0,0 +1,23 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def print_star(length):
+    if length == 1:
+        return ['*']
+    
+    stars = print_star(length//3)
+    tmp = []
+
+    for s in stars:
+        tmp.append(s*3)
+    for s in stars:
+        tmp.append(s+' '*(length//3)+s)
+    for s in stars:
+        tmp.append(s*3)
+    
+    return tmp
+
+n = int(input())
+for s in print_star(n):
+    print(s)
---
+++ week12_divide_conquer/2448 별 찍기 11.py
@@ -0,0 +1,22 @@
+n=int(input())
+star = ['  *  ', ' * * ', '*****']
+n = n//3
+
+def print_star(star):
+    length = len(star)
+    for i in range(length):
+        star.append(star[i] + ' ' + star[i])
+        star[i] = ' '*length + star[i] + ' '*length
+    
+    return star
+
+cnt = 0
+while n > 1:
+    n = n//2
+    cnt += 1
+
+for i in range(cnt):
+    star = print_star(star)
+
+for s in star:
+    print(s)
