5d3d7a95b5473dd635be83e90b3e4c1ab2ea8c4a
math0917 <math0917@naver.com>
2021-09-28 11:59:26 +0900

    DAY 63

---
+++ week12_divide and conquer/2339(100%후 틀렸습니다...).py
@@ -0,0 +1,46 @@
+import sys
+
+def divide (start_row, start_col, finish_row, finish_col, horizonal):
+    divide_complete ,jewel, trash =0,0,0
+    for i in range(start_row,finish_row):
+        for j in range(start_col, finish_col):
+            if arr[i][j] == 1:
+                trash += 1
+                if horizonal:
+                    divide_complete += divide(start_row,start_col, i,finish_col, not(horizonal)) * divide(i+1,start_col,finish_row,finish_col,not(horizonal))
+                else:
+                    divide_complete += divide(start_row,start_col, finish_row,j,not(horizonal)) * divide(start_row,j+1,finish_row,finish_col, not(horizonal))
+            elif arr[i][j] == 2:
+                jewel+=1
+    if trash == 0:
+        if jewel == 1:
+            return 1
+        else:
+            return 0
+    return divide_complete
+n = int(sys.stdin.readline())
+
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+trash = 0
+jewel = 0
+
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1:
+            trash += 1
+
+        elif arr[i][j] == 2:
+            jewel += 1
+
+if trash == 0: 
+    
+    
+    print(-1)
+    sys.exit()
+cnt = divide(0,0,n,n,0) + divide(0,0,n,n,1)
+if cnt :
+    print(cnt)
+else:
+    print(-1)
