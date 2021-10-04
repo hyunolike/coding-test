4e14a0992f85d734a3ef02a99a4b49da447f88e3
ghgit1798 <ghgoo1798@gmail.com>
2021-10-04 12:53:05 +0900

    Day70

---
+++ week13_two_pointer/2230 수 고르기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+a = list(int(input()) for _ in range(n))
+answer = 2e9
+l, r = 0, 1
+diff = 0
+a.sort()
+
+while True:
+    if l >= n or r >= n:
+        break
+    diff = a[r]-a[l]
+    if diff < m:
+        r += 1
+        continue
+    elif diff == m:
+        answer = diff
+        break
+    answer = min(answer, diff)
+    l += 1
+
+print(answer)
