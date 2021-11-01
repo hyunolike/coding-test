180cb19c020b714bb4304ecbd48a194cae743428
ghgit1798 <ghgoo1798@gmail.com>
2021-11-01 14:29:50 +0900

    wee15 String 문제풀이

--- week15_string/1316 그룹 단어 체커.py
+++ week15_string/1316 그룹 단어 체커.py
@@ -2,15 +2,15 @@ n=int(input())
 answer=0
 for _ in range(n):
     word=input()
-    ch=[word[0]]
-    group=True
+    space=set()
+    flag=True
     for i in range(1, len(word)):
         if word[i]!=word[i-1]:
-            if word[i] in ch:
-                group=False
+            if word[i] in space:
+                flag=False
                 break
             else:
-                ch.append(word[i])
-    if group:
+                space.add(word[i-1])
+    if flag:
         answer+=1
 print(answer)
