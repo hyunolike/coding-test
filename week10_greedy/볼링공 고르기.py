4ba0f73ed0615d7d7319b2d0a078928cf6917e0b
ghgit1798 <ghgoo1798@gmail.com>
2021-11-04 13:10:01 +0900

    Greedy 이코테 문제풀이 코드 수정

--- week10_greedy/볼링공 고르기.py
+++ week10_greedy/볼링공 고르기.py
@@ -22,6 +22,6 @@ result=0
 # 1부터 m까지의 각 무게에 대하여 처리
 for i in range(1, m+1):
     n-=array[i] # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
-    result += array[i]*n # B가 선택하는 경우의 수와 곱하기
+    result += array[i]*n # B가 선택하는 경우의 수(n)와 곱하기
 
 print(result)
