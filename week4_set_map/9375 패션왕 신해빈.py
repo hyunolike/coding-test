e90edeec8a2d6866a3077efdd7b1daed8de7d994
ghgit1798 <ghgoo1798@gmail.com>
2022-06-29 23:08:02 +0900

    9375 패션왕 김해빈 defaultdict를 활용한 더 깔끔한 풀이 업데이트

--- week4_set_map/9375 패션왕 신해빈.py
+++ week4_set_map/9375 패션왕 신해빈.py
@@ -1,18 +1,37 @@
+from collections import defaultdict
+
 test = int(input())
 
 for _ in range(test):
-    data = dict()
     n = int(input())
+    data = defaultdict(list)
     cnt = 1
-
+    
     for _ in range(n):
         name, kind = input().split()
-        if kind not in data.keys():
-            name = [name]
-            data[kind] = name
-        else:
-            data[kind].append(name)
-    for k, v in data.items(): 
+        data[kind].append(name)
+    
+    for k, v in data.items():
         cnt *= len(v)+1
     
     print(cnt-1)
+
+
+# test = int(input())
+
+# for _ in range(test):
+#     n = int(input())
+#     data = dict()
+#     cnt = 1
+
+#     for _ in range(n):
+#         name, kind = input().split()
+#         if kind not in data.keys():
+#             name = [name]
+#             data[kind] = name
+#         else:
+#             data[kind].append(name)
+#     for k, v in data.items(): 
+#         cnt *= len(v)+1
+    
+#     print(cnt-1)
